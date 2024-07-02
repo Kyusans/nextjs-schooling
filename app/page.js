9dbad68d875117e7e1124fd0268fc91fe93cece7
Mel Macario <xmelmacario@gmail.com>
2024-07-02 10:48:56 +0800

    added carousel slides

--- app/page.js
+++ app/page.js
@@ -1,33 +1,69 @@
-"use client"
-import { Carousel } from "react-bootstrap";
+"use client";
+import Image from 'next/image';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Home() {
   return (
     <>
-      <div className="bg-zinc-700 w-full h-6">
-      <Carousel>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      <div className='bg-zinc-800 '>
+        <Carousel data-bs-theme="light">
+          <Carousel.Item>
+            <div className="flex items-center">
+              <div className="relative w-1/2 h-96">
+                <Image
+                  className="object-cover"
+                  src={"/assets/images/1.jpg"}
+                  layout="fill"
+                  alt="First slide"
+                />
+              </div>
+              <div className="w-1/2 p-5 text-white text-center">
+                <h3>It&apos;s me</h3>
+                <h1>Mel Macario</h1>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="flex items-center ">
+              <div className="relative w-1/2 h-96">
+                <Image
+                  className="object-cover"
+                  src={"/assets/images/2.JPG"}
+                  layout="fill"
+                  alt="Second slide"
+                />
+              </div>
+              <div className="w-1/2 p-4 text-white text-center">
+                <h1>Front End Developer</h1>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="flex items-center">
+              <div className="relative w-1/2 h-96">
+                <Image
+                  className="object-cover"
+                  src={"/assets/images/3.JPG"}
+                  layout="fill"
+                  alt="Third slide"
+                />
+              </div>
+              <div className="w-1/2 p-4 text-white text-center">
+                <h1>Back End Developer</h1>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
       </div>
+
+      <div className='text-center mt-5'>
+        <p className="mt-3 italic">
+          &ldquo;If you break your legs, it&apos;s hard to cook orangutan. So don&apos;t break your legs.&rdquo;<br />
+          <span className="font-bold und">- Mel Macario</span>
+        </p>
+      </div>
+
+
     </>
   );
 }
